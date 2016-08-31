86b2a7eaee8a8fe7712a6d8a56105d63d714f7ba
Chad H <glaserc@onid.oregonstate.edu>
2016-08-31 08:21:52 -0700

    added comments

--- DropIt/DropIt.js
+++ DropIt/DropIt.js
@@ -1,6 +1,15 @@
+/*
+Free Code Camp Drop it
+Drop the elements of an array (first argument), starting from the front, until the predicate (second argument) returns true.
+
+The second argument, func, is a function you'll use to test the first elements of the array to decide if you should drop it or not.
+
+Return the rest of the array, otherwise return an empty array.
+*/
 function dropElements(arr, func) {
   // Drop them elements.
-  
+  //For the return array, take the array as input, start where k left off and go to the end.
+  //Pushing elements onto the new array. 
   function notDropped(thisArr, first, length){
      var newArr = [];
      for(var j = first; j < length; j++){
@@ -8,10 +17,13 @@ function dropElements(arr, func) {
      }
      return newArr;
   }
- 
+ //i for iteration
   var i = 0;
+  //k for counting where to start the new array
   var k = 0;
+  //empty array for when there is nothing to return 
   var emp = [];
+  //loop through elements seeking the true element, breaking when found.
   for(i; i < arr.length; i++){
         if(func(arr[i])){
             break;
